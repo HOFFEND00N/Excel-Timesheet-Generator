34667aeb6e78878ec0c80c03a808c3e5cbc9339d
Ivan Petrov <Ivan.Petrov@confirmit.com>
2021-04-26 13:32:22 +0300

    change test, now add several values

--- tests/makeTableRow.test.ts
+++ tests/makeTableRow.test.ts
@@ -13,14 +13,15 @@ test("make Table Row, pass empty values, expect empty array", () => {
   expect(actualRow).toEqual(expectedRow);
 });
 
-test("make Table Row, pass one value, expect one row", () => {
+test("make Table Row, pass two value, expect one row with two values", () => {
   const expectedRow: CommonCell[] = [
     new TableCell({ point: { column: 1, row: 1 }, value: "a", styles: [] }),
+    new TableCell({ point: { column: 2, row: 1 }, value: "abs", styles: [] }),
   ];
 
   const actualRow = makeTableRow({
     startPoint: { column: 1, row: 1 },
-    values: ["a"],
+    values: ["a", "abs"],
   });
 
   expect(actualRow).toEqual(expectedRow);
