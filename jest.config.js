23d09ded9825d15b3287989d55228f6702f5450d
Ivan Petrov <Ivan.Petrov@confirmit.com>
2021-07-23 15:12:58 +0300

    define files&folders for jest tests coverage

--- jest.config.js
+++ jest.config.js
@@ -8,12 +8,18 @@ const config = {
       lines: 100,
       statements: 100,
     },
-    src: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
   },
+  collectCoverageFrom: [
+    "**/*.ts",
+    "!**/node_modules/**",
+    "!./src/app.ts",
+    "!./src/XMLGeneratingFunctions/**",
+    "!./src/classes/**",
+    "!./src/tableBuildingFunctions/fetchJiraUserTasks.ts",
+    "!./src/tableBuildingFunctions/getCredentials.ts",
+    "!./src/tableBuildingFunctions/getNonWorkingHoursFile.ts",
+    "!./src/tableBuildingFunctions/getWorkingHoursForMonth.ts",
+    "!./src/tableBuildingFunctions/types.ts",
+  ],
 };
 export default config;
