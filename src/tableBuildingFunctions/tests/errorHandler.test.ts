e7e1c6efd84c6ee87e9aac04ca8b3c3f84d119cf
Ivan Petrov <Ivan.Petrov@confirmit.com>
2021-08-10 15:32:17 +0300

    mock functions in tests, instead of external varaible

--- src/tableBuildingFunctions/tests/errorHandler.test.ts
+++ src/tableBuildingFunctions/tests/errorHandler.test.ts
@@ -1,25 +1,28 @@
 import { errorHandler } from "../errorHandler";
 
 test("pass function with , expect to return 1", async () => {
-  const expected = 1;
-
-  const actual = await errorHandler(async () => {
+  const callback = jest.fn().mockImplementation(async () => {
     return 1;
   });
 
-  expect(actual).toEqual(expected);
+  const actual = await errorHandler(callback);
+
+  expect(callback).toBeCalledTimes(1);
+  expect(actual).toEqual(1);
 });
 
 test("pass function with one caught error, expect to return 1 on second iteration", async () => {
-  const expected = true;
   let exceptionThrown = false;
-
-  const actual = await errorHandler(async () => {
+  const callback = jest.fn().mockImplementation(async () => {
     if (!exceptionThrown) {
       exceptionThrown = true;
       throw new Error("Error!");
-    } else return exceptionThrown;
+    }
+    return 1;
   });
 
-  expect(actual).toEqual(expected);
+  const actual = await errorHandler(callback);
+
+  expect(callback).toBeCalledTimes(2);
+  expect(actual).toEqual(1);
 });
