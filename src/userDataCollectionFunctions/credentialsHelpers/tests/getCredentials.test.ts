8cd479637fd6c9f167922c672acad63ca422899c
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-12-23 14:30:52 +0300

    refactor: remove describe block in tests

--- src/userDataCollectionFunctions/credentialsHelpers/tests/getCredentials.test.ts
+++ src/userDataCollectionFunctions/credentialsHelpers/tests/getCredentials.test.ts
@@ -7,83 +7,81 @@ jest.mock("../getCredentialsFromEnvironment");
 jest.mock("../getCredentialsFromCLI");
 jest.mock("../../areJiraCredentialsCorrect");
 
-describe("get credentials", () => {
-  test("should return credentials from environment", async () => {
-    (getCredentialsFromEnvironment as jest.Mock).mockReturnValue({ login: "login", password: "password" });
-    (areJiraCredentialsCorrect as jest.Mock).mockReturnValue(true);
-    const expectedLogin = "login",
-      expectedPassword = "password";
+test("should return credentials from environment", async () => {
+  (getCredentialsFromEnvironment as jest.Mock).mockReturnValue({ login: "login", password: "password" });
+  (areJiraCredentialsCorrect as jest.Mock).mockReturnValue(true);
+  const expectedLogin = "login",
+    expectedPassword = "password";
 
-    const { login: actualLogin, password: actualPassword } = await getCredentials({
-      env: { loginKey: "", passwordKey: "" },
-    });
-
-    expect(actualLogin).toEqual(expectedLogin);
-    expect(actualPassword).toEqual(expectedPassword);
+  const { login: actualLogin, password: actualPassword } = await getCredentials({
+    env: { loginKey: "", passwordKey: "" },
   });
 
-  test("should return credentials from config, when credentials from environment are incorrect", async () => {
-    (getCredentialsFromEnvironment as jest.Mock).mockReturnValue({ login: "bad login", password: "password" });
-    (areJiraCredentialsCorrect as jest.Mock).mockImplementation(({ login }) => {
-      return login !== "bad login";
-    });
-    const expectedLogin = "login",
-      expectedPassword = "password";
+  expect(actualLogin).toEqual(expectedLogin);
+  expect(actualPassword).toEqual(expectedPassword);
+});
 
-    const { login: actualLogin, password: actualPassword } = await getCredentials({
-      login: "login",
-      password: "password",
-      env: { loginKey: "", passwordKey: "" },
-    });
+test("should return credentials from config, when credentials from environment are incorrect", async () => {
+  (getCredentialsFromEnvironment as jest.Mock).mockReturnValue({ login: "bad login", password: "password" });
+  (areJiraCredentialsCorrect as jest.Mock).mockImplementation(({ login }) => {
+    return login !== "bad login";
+  });
+  const expectedLogin = "login",
+    expectedPassword = "password";
 
-    expect(actualLogin).toEqual(expectedLogin);
-    expect(actualPassword).toEqual(expectedPassword);
+  const { login: actualLogin, password: actualPassword } = await getCredentials({
+    login: "login",
+    password: "password",
+    env: { loginKey: "", passwordKey: "" },
   });
 
-  test("should return credentials from CLI, when credentials from environment and config are incorrect", async () => {
-    (getCredentialsFromEnvironment as jest.Mock).mockReturnValue({ login: "bad login", password: "password" });
-    (getCredentialsFromCLI as jest.Mock).mockReturnValue({ login: "login", password: "password" });
-    (areJiraCredentialsCorrect as jest.Mock).mockImplementation(({ login }) => {
-      return login !== "bad login";
-    });
-    const expectedLogin = "login",
-      expectedPassword = "password";
+  expect(actualLogin).toEqual(expectedLogin);
+  expect(actualPassword).toEqual(expectedPassword);
+});
 
-    const { login: actualLogin, password: actualPassword } = await getCredentials({
-      login: "bad login",
-      password: "password",
-      env: { loginKey: "", passwordKey: "" },
-    });
+test("should return credentials from CLI, when credentials from environment and config are incorrect", async () => {
+  (getCredentialsFromEnvironment as jest.Mock).mockReturnValue({ login: "bad login", password: "password" });
+  (getCredentialsFromCLI as jest.Mock).mockReturnValue({ login: "login", password: "password" });
+  (areJiraCredentialsCorrect as jest.Mock).mockImplementation(({ login }) => {
+    return login !== "bad login";
+  });
+  const expectedLogin = "login",
+    expectedPassword = "password";
 
-    expect(actualLogin).toEqual(expectedLogin);
-    expect(actualPassword).toEqual(expectedPassword);
+  const { login: actualLogin, password: actualPassword } = await getCredentials({
+    login: "bad login",
+    password: "password",
+    env: { loginKey: "", passwordKey: "" },
   });
 
-  test("should return credentials from config, when credentials from environment are not present", async () => {
-    (getCredentialsFromCLI as jest.Mock).mockReturnValue({ login: "login", password: "password" });
-    (areJiraCredentialsCorrect as jest.Mock).mockImplementation(({ login }) => {
-      return login !== "bad login";
-    });
-    const expectedLogin = "login",
-      expectedPassword = "password";
+  expect(actualLogin).toEqual(expectedLogin);
+  expect(actualPassword).toEqual(expectedPassword);
+});
 
-    const { login: actualLogin, password: actualPassword } = await getCredentials({
-      login: "login",
-      password: "password",
-    });
+test("should return credentials from config, when credentials from environment are not present", async () => {
+  (getCredentialsFromCLI as jest.Mock).mockReturnValue({ login: "login", password: "password" });
+  (areJiraCredentialsCorrect as jest.Mock).mockImplementation(({ login }) => {
+    return login !== "bad login";
+  });
+  const expectedLogin = "login",
+    expectedPassword = "password";
 
-    expect(actualLogin).toEqual(expectedLogin);
-    expect(actualPassword).toEqual(expectedPassword);
+  const { login: actualLogin, password: actualPassword } = await getCredentials({
+    login: "login",
+    password: "password",
   });
 
-  test("should return credentials from CLI, when credentials from environment and config are not present", async () => {
-    (getCredentialsFromCLI as jest.Mock).mockReturnValue({ login: "login", password: "password" });
-    const expectedLogin = "login",
-      expectedPassword = "password";
+  expect(actualLogin).toEqual(expectedLogin);
+  expect(actualPassword).toEqual(expectedPassword);
+});
 
-    const { login: actualLogin, password: actualPassword } = await getCredentials({});
+test("should return credentials from CLI, when credentials from environment and config are not present", async () => {
+  (getCredentialsFromCLI as jest.Mock).mockReturnValue({ login: "login", password: "password" });
+  const expectedLogin = "login",
+    expectedPassword = "password";
 
-    expect(actualLogin).toEqual(expectedLogin);
-    expect(actualPassword).toEqual(expectedPassword);
-  });
+  const { login: actualLogin, password: actualPassword } = await getCredentials({});
+
+  expect(actualLogin).toEqual(expectedLogin);
+  expect(actualPassword).toEqual(expectedPassword);
 });
--- src/userDataCollectionFunctions/tests/getWorkingHoursByEmployeesUsername.test.ts
+++ src/userDataCollectionFunctions/tests/getWorkingHoursByEmployeesUsername.test.ts
@@ -1,28 +1,26 @@
 import { HoursByEmployees } from "../../tableBuildingFunctions/types";
 import { getWorkingHoursByEmployeesUsername } from "../getWorkingHoursByEmployeesUsername";
 
-describe("get working hours by employees username", () => {
-  test("should return one employee with specific working hours and one with default", () => {
-    const expectedWorkingHoursByEmployeesUsername: HoursByEmployees = {
-      "employee 1": 160,
-      "employee 2": 120,
-    };
+test("should return one employee with specific working hours and one with default", () => {
+  const expectedWorkingHoursByEmployeesUsername: HoursByEmployees = {
+    "employee 1": 160,
+    "employee 2": 120,
+  };
 
-    const actualWorkingHoursByEmployeesUsername = getWorkingHoursByEmployeesUsername({
-      workingHoursPerMonth: 160,
-      team: [
-        {
-          jiraUsername: "employee 1",
-          name: "employee 1",
-        },
-        {
-          jiraUsername: "employee 2",
-          name: "employee 2",
-          workingHoursPerMonth: 120,
-        },
-      ],
-    });
-
-    expect(actualWorkingHoursByEmployeesUsername).toEqual(expectedWorkingHoursByEmployeesUsername);
+  const actualWorkingHoursByEmployeesUsername = getWorkingHoursByEmployeesUsername({
+    workingHoursPerMonth: 160,
+    team: [
+      {
+        jiraUsername: "employee 1",
+        name: "employee 1",
+      },
+      {
+        jiraUsername: "employee 2",
+        name: "employee 2",
+        workingHoursPerMonth: 120,
+      },
+    ],
   });
+
+  expect(actualWorkingHoursByEmployeesUsername).toEqual(expectedWorkingHoursByEmployeesUsername);
 });
