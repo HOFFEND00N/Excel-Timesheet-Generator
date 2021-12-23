8cdd8c071f0396571768054496ca13571629eb1e
Ivan.Petrov <Y@eyNM@DG2vmX2c>
2021-12-23 14:27:51 +0300

    feat: add tests

---
+++ src/userDataCollectionFunctions/nonWorkingHoursHelpers/tests/getNonWorkingHoursFile.test.ts
@@ -0,0 +1,43 @@
+import { getNonWorkingHoursFile } from "../getNonWorkingHoursFile";
+import { readNonWorkingHoursFile } from "../readNonWorkingHoursFile";
+import { getPathToNonWorkingHoursFileFromCLI } from "../getPathToNonWorkingHoursFileFromCLI";
+
+jest.mock("../readNonWorkingHoursFile");
+jest.mock("../getPathToNonWorkingHoursFileFromCLI");
+
+describe("get non working hours file", () => {
+  test("should get file, when path in config", async () => {
+    (readNonWorkingHoursFile as jest.Mock).mockReturnValue([["test"]]);
+    const expectedFile = [["test"]];
+
+    const actualFile = await getNonWorkingHoursFile("");
+
+    expect(actualFile).toEqual(expectedFile);
+  });
+
+  test("should get file, when path in config incorrect, then get correct path from CLI", async () => {
+    (readNonWorkingHoursFile as jest.Mock).mockImplementation((path: string) => {
+      if (path !== "bad path") {
+        return [["test"]];
+      } else {
+        throw "error";
+      }
+    });
+    (getPathToNonWorkingHoursFileFromCLI as jest.Mock).mockReturnValue("test");
+    const expectedFile = [["test"]];
+
+    const actualFile = await getNonWorkingHoursFile("bad path");
+
+    expect(actualFile).toEqual(expectedFile);
+  });
+
+  test("should get file, when path in config incorrect, then get correct path from CLI", async () => {
+    (readNonWorkingHoursFile as jest.Mock).mockReturnValue([["test"]]);
+    (getPathToNonWorkingHoursFileFromCLI as jest.Mock).mockReturnValue("");
+    const expectedFile = [["test"]];
+
+    const actualFile = await getNonWorkingHoursFile("path");
+
+    expect(actualFile).toEqual(expectedFile);
+  });
+});
---
+++ src/userDataCollectionFunctions/tests/getWorkingHoursByEmployeesUsername.test.ts
@@ -0,0 +1,28 @@
+import { HoursByEmployees } from "../../tableBuildingFunctions/types";
+import { getWorkingHoursByEmployeesUsername } from "../getWorkingHoursByEmployeesUsername";
+
+describe("get working hours by employees username", () => {
+  test("should return one employee with specific working hours and one with default", () => {
+    const expectedWorkingHoursByEmployeesUsername: HoursByEmployees = {
+      "employee 1": 160,
+      "employee 2": 120,
+    };
+
+    const actualWorkingHoursByEmployeesUsername = getWorkingHoursByEmployeesUsername({
+      workingHoursPerMonth: 160,
+      team: [
+        {
+          jiraUsername: "employee 1",
+          name: "employee 1",
+        },
+        {
+          jiraUsername: "employee 2",
+          name: "employee 2",
+          workingHoursPerMonth: 120,
+        },
+      ],
+    });
+
+    expect(actualWorkingHoursByEmployeesUsername).toEqual(expectedWorkingHoursByEmployeesUsername);
+  });
+});
